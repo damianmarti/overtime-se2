de701e8f3a33b2431f278a22ef88b498f9233846
Damian <damianmarti@gmail.com>
2025-10-13 20:46:59 -0300

    Add SportsAMMV2 external contract

--- packages/nextjs/contracts/externalContracts.ts
+++ packages/nextjs/contracts/externalContracts.ts
@@ -1,3 +1,5 @@
+import { Abi } from "abitype";
+import SportsAMMV2ABI from "~~/contracts/abis/SportsAMMV2.json";
 import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
 
 /**
@@ -11,6 +13,13 @@ import { GenericContractsDeclaration } from "~~/utils/scaffold-eth/contract";
  *   },
  * } as const;
  */
-const externalContracts = {} as const;
+const externalContracts = {
+  10: {
+    SportsAMMV2: {
+      address: "0xFb4e4811C7A811E098A556bD79B64c20b479E431",
+      abi: SportsAMMV2ABI as Abi,
+    },
+  },
+} as const;
 
 export default externalContracts satisfies GenericContractsDeclaration;
